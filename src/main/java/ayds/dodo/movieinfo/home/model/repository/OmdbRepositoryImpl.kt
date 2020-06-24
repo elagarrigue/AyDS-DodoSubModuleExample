8ff1b86cdc2e7cba1f6f07057f2a919f15cad795
Emmanuel Lagarrigue <elagarrigue@gmail.com>
2020-06-24 09:11:38 -0300

    Move omdb service to submodule

--- src/main/java/ayds/dodo/movieinfo/home/model/repository/OmdbRepositoryImpl.kt
+++ src/main/java/ayds/dodo/movieinfo/home/model/repository/OmdbRepositoryImpl.kt
@@ -1,8 +1,10 @@
 package ayds.dodo.movieinfo.home.model.repository
 
 import ayds.dodo.movieinfo.home.model.entities.OmdbMovie
-import ayds.dodo.movieinfo.home.model.repository.external.ExternalService
+import ayds.dodo.movieinfo.home.model.entities.Rating
+import ayds.dodo.movieinfo.omdb.external.ExternalService
 import ayds.dodo.movieinfo.home.model.repository.local.LocalStorage
+import ayds.dodo.movieinfo.omdb.external.omdb.Movie
 
 internal class OmdbRepositoryImpl(
     private val localStorage: LocalStorage,
@@ -16,7 +18,10 @@ internal class OmdbRepositoryImpl(
             movie != null -> markMovieAsLocal(movie)
             else -> {
                 try {
-                    movie = externalService.getMovie(title)
+                    val dataMovie = externalService.getMovie(title)
+
+                    movie = dataMovie.toOmdbMovie()
+
                     localStorage.saveMovie(title, movie)
                 } catch (e: Exception) {
                     e.printStackTrace()
@@ -28,5 +33,23 @@ internal class OmdbRepositoryImpl(
 
     private fun markMovieAsLocal(movie: OmdbMovie) {
         movie.isLocallyStoraged = true
+    }
+
+    private fun Movie.toOmdbMovie() : OmdbMovie  {
+        val omdbMovie =  OmdbMovie()
+        omdbMovie.title = title
+        omdbMovie.year = year
+        omdbMovie.plot = plot
+        omdbMovie.director = director
+        omdbMovie.actors = actors
+        omdbMovie.posterUrl = posterUrl
+        omdbMovie.isLocallyStoraged = false
+        omdbMovie.ratings = ratings.map {
+            Rating().apply {
+                source = it.source
+                value = it.value
+            }
+        }
+        return omdbMovie
     }
 }
--- src/main/java/ayds/dodo/movieinfo/home/model/repository/external/ExternalService.kt
+++
@@ -1,8 +0,0 @@
-package ayds.dodo.movieinfo.home.model.repository.external
-
-import ayds.dodo.movieinfo.home.model.entities.OmdbMovie
-
-interface ExternalService {
-
-    fun getMovie(title: String): OmdbMovie
-}
